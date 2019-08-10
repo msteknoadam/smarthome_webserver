cc1ad957bcc371dd6b4830a7141418cb6a90abbf
msteknoadam <yesilyurt.gok@gmail.com>
2019-08-10 12:04:46 +0300

    Make sure raspiclient reconnects when the server is down/connection is lost etc. etc.

--- raspberryclient/client.ts
+++ raspberryclient/client.ts
@@ -2,25 +2,39 @@ import * as io from "socket.io-client";
 
 /* Python sux */
 
-const socket = io("http://localhost:3000");
+let socket: SocketIOClient.Socket;
 
-socket.on("connection", () => {
-	console.log("Successfully connected to the server");
-});
-
-socket.on("disconnect", () => {
-	console.log("Disconnected from the server");
-});
-
-socket.on("initialize", (data: object) => {
-	console.log("initialize", data);
-});
-
-socket.on("stateChange", (newStates: object) => {
-	console.log("newStates", newStates);
-	Object.keys(newStates).forEach(newStateKey => {
+const connectToSocket = () => {
+	if (socket) socket.close();
+	socket = undefined;
+	console.log("Attempting to connect to websocket.");
+	socket = io("http://localhost:3000");
+	socket.on("connect", () => {
 		console.log(
-			`Changed state of '${newStateKey}' to '${newStates[newStateKey]}'`
+			`Successfully connected to the server with id '${socket.id}'`
 		);
 	});
-});
+
+	socket.on("disconnect", () => {
+		console.log("Disconnected from the server, trying to reconnect.");
+		socket.close();
+		connectToSocket();
+	});
+
+	socket.on("initialize", (data: object) => {
+		console.log("initialize", data);
+	});
+
+	socket.on("stateChange", (newStates: object) => {
+		console.log("newStates", newStates);
+		Object.keys(newStates).forEach(newStateKey => {
+			console.log(
+				`Changed state of '${newStateKey}' to '${
+					newStates[newStateKey]
+				}'`
+			);
+		});
+	});
+};
+
+connectToSocket();
